55eb3d9331ea095b412b6ae9776429b5f66d387b
sascha bittlingmayer <sascha@bittlingmayer.com>
2025-02-20 21:21:11 +0100

    Add pwa to project

--- 7FlipCounter/src/main.ts
+++ 7FlipCounter/src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {importProvidersFrom, isDevMode} from '@angular/core';
+import {ServiceWorkerModule} from '@angular/service-worker';
+
+const config = {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers ?? [],
+    importProvidersFrom(
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+      })
+    )
+  ]
+};
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # 7FlipCounter
 
-- wer ist der Startspieler, immer eins weiter in der position
-Mann muss auf neues spiel klicken, dann öffnet sich ein dialog mit allen spielern,
-hier muss ein Startspielr gewählt werden, danach wird reihum der startspieler grün.
-- bei Neuses Spiel klick alle Scores löschen
-- score muss in den spinner
+
+
 - History
